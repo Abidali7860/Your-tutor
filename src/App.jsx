1f7203059bb20cdb7157bdfbd3f61971d0b78a7b
Abidali7860 <161067504+Abidali7860@users.noreply.github.com>
2025-02-21 15:39:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs'
 import Img1 from "./assets/banner1.png";
 import Img2 from "./assets/banner2.png";
 import Banner from './components/Banner/Banner'
-import SubjectCard from './components/SubjectCard/Subjectcard'
+import SubjectCard from './components/SubjectCard/SubjectCard'
 import Testimonial from './components/Testimonial/Testimonial'
 import Footer from './components/Footer/Footer'
 
